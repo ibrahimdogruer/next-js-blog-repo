fc95491aade084e0e9c6c8c24bb56da1ad268938
İbrahim Doğruer <ibrahim@assayazilim.com>
2021-05-17 23:05:51 +0300

    responsive ui

--- components/header.js
+++ components/header.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
+import Light from "../icons/light";
 import Link from "next/link";
+import Moon from "../icons/moon";
 import { useRouter } from "next/router";
 import { useTheme } from "next-themes";
 
@@ -35,7 +37,9 @@ export default function Header() {
             <Link href={url} key={id}>
               <a
                 className={`hover:text-gray-700 text-lg ${
-                  pathname === url ? "text-blue-500 dark:text-yellow-500" : "text-gray-800 dark:text-white"
+                  pathname === url
+                    ? "text-blue-500 dark:text-yellow-500"
+                    : "text-gray-800 dark:text-white"
                 }`}
               >
                 {text}
@@ -49,47 +53,7 @@ export default function Header() {
         dark:bg-black dark:border-white dark:hover:bg-yellow-500"
           onClick={switchTheme}
         >
-          {theme === "dark" ? (
-            <svg
-              stroke="currentColor"
-              fill="none"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="css-54dw7o-DefaultMode e1llm6650"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Switch to light mode</title>
-              <circle cx="12" cy="12" r="5"></circle>
-              <line x1="12" y1="1" x2="12" y2="3"></line>
-              <line x1="12" y1="21" x2="12" y2="23"></line>
-              <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-              <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-              <line x1="1" y1="12" x2="3" y2="12"></line>
-              <line x1="21" y1="12" x2="23" y2="12"></line>
-              <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-              <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-            </svg>
-          ) : (
-            <svg
-              stroke="currentColor"
-              fill="none"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="css-11fnkgj-DarkMode e1llm6651"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Switch to dark mode</title>
-              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-            </svg>
-          )}
+          {theme === "dark" ? <Light size={16} /> : <Moon size={16} />}
         </button>
       </nav>
     </header>
--- components/post-meta.js
+++ components/post-meta.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function PostMeta({ post }) {
   return (
-    <div className="flex text-gray-500 mt-2 justify-between">
-      <div>
+    <div className="flex text-gray-500 mt-2 justify-between flex-col md:flex-row">
+      <div className="flex-row">
         {post.frontMatter.author.length ? (
-          <span className="inline-block mr-4 dark:text-gray-500">
+          <span className="inline-block mr-2 md:mr-4 dark:text-gray-500">
             Yazar{" "}
             {post.frontMatter.author.map((author, index) => (
               <strong
@@ -18,7 +18,7 @@ export default function PostMeta({ post }) {
             ))}
           </span>
         ) : null}
-        <span className="inline-block mr-2 dark:text-gray-500">
+        <span className="inline-block mr-2 md:mr-4 dark:text-gray-500">
           Kategori{" "}
           {post.frontMatter.category.map((category, index) => (
             <span
@@ -33,7 +33,7 @@ export default function PostMeta({ post }) {
           ))}
         </span>
       </div>
-      <span className="mx-6">
+      <span className="mx-6 text-right">
         <span>
           {DateTime.fromSQL(post.frontMatter.date).toFormat("DDD", {
             locale: "tr",
